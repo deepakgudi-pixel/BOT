344de6b51a1f24527c0d368cae3de1dd9d56d122
deepakgudi-pixel <japanesen52018@gmail.com>
2023-04-14 16:15:16 +0530

    final

---
+++ main.js
@@ -0,0 +1,152 @@
+import anime from "animejs";
+import "./style.css";
+
+function init() {
+  let current = 0;
+  let directionForward = true;
+  const items = document.querySelectorAll(".card-slider .items .item");
+  const prevButton = document.querySelector(".card-slider .prev");
+  const nextButton = document.querySelector(".card-slider .next");
+
+  const getCurrentItem = () => {
+    return;
+  };
+
+  const getCard = (item) => {
+     return item.querySelector(".card");
+  };
+
+  const getTitle = (item) => {
+    return item.querySelector(".title span");
+  };
+
+  const setItems = () => {
+    items.forEach((item, index) => {
+      getTitle(item).innerHTML = getTitle(item).textContent.replace(
+        /\S/g,
+        "<span class='letter'>$&</span>"
+      );
+
+      if (index === current) return;
+
+      anime.set(getCard(item), {
+        translateX: "100vw",
+      });
+
+      anime.set(getTitle(item).querySelectorAll(".letter"), {
+        clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+      });
+    });
+
+    anime.set(items[current], {
+      translateX: 0,
+      opacity: 1,
+    });
+  };
+
+  const animate = {
+    in(item) {
+      const card = getCard(item);
+      const title = getTitle(item);
+
+      const tl = anime.timeline({
+        duration: 1000,
+        easing: "easeOutExpo",
+      });
+
+      tl.add({
+        targets: card,
+        //   opacity: [0, 1],
+        translateX: directionForward ? ["100vw", 0] : ["-100vw", 0],
+        rotate: [40, 0],
+      }).add(
+        {
+          targets: title.querySelectorAll(".letter"),
+          clipPath: [
+            "polygon(0 0, 100% 0, 100% 0, 0 0)",
+            "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+          ],
+          translateY: directionForward ? ["1em", 0] : ["-1em", 0],
+          delay: anime.stagger(40),
+        },
+        "-=1000"
+      );
+
+      return tl;
+    },
+
+    out(item) {
+      const card = getCard(item);
+      const title = getTitle(item);
+
+      const tl = anime.timeline({
+        duration: 1000,
+        easing: "cubicBezier(0.86,0,0.07,1);",
+      });
+
+      tl.add({
+        targets: card,
+        translateX: directionForward ? [0, "-100vw"] : [0, "100vw"],
+        rotate: [0, -40],
+      }).add(
+        {
+          targets: title.querySelectorAll(".letter"),
+          clipPath: [
+            "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)",
+            "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+          ],
+          translateY: directionForward ? [0, "-1em"] : [0, "1em"],
+          delay: anime.stagger(40),
+        },
+        "-=1000"
+      );
+
+      return tl;
+    },
+  };
+
+  function updateClasses() {
+    items.forEach((item, index) => {
+      if (index == current) {
+        item.classList.add("is-active");
+      } else {
+        item.classList.remove('is-active');
+      }
+    });
+  }
+
+  function next() {
+    if (!directionForward) {
+      directionForward = !directionForward;
+    }
+
+    animate.out(items[current]);
+    current = (current + 1) % items.length;
+
+    setTimeout(function () {
+      animate.in(items[current]);
+    }, 500);
+    updateClasses();
+  }
+
+  function previous() {
+    if (directionForward) {
+      directionForward = !directionForward;
+    }
+
+    animate.out(items[current]);
+    current = (current - 1 + items.length) % items.length;
+
+    setTimeout(function () {
+      animate.in(items[current]);
+    }, 500);
+    updateClasses();
+  }
+
+  nextButton.addEventListener("click", next);
+  prevButton.addEventListener("click", previous);
+
+  setItems();
+}
+
+document.addEventListener("DOMContentLoaded", init);
